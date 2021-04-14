9a41d033ce473a02c2eb564dfad7099e2c7e32f2
karashiiro <49822414+karashiiro@users.noreply.github.com>
2021-04-14 12:08:06 -0700

    The rest of the owl

---
+++ Dalamud-Checksummer.ps1
@@ -0,0 +1,38 @@
+if ($Args.Count -lt 2) {
+    Write-Output "Please provide a plugin zip as the first argument of this command, and a metadata file as the second."
+    return
+}
+
+$pluginZip = $Args[0]
+$metaFile = $Args[1]
+$meta = Get-Content -Path $metaFile | ConvertFrom-JSON
+
+# Unzip archive and get the DLL hash from it
+Expand-Archive -Path $pluginZip -DestinationPath unpack/ -Force
+Set-Location unpack
+$hash1 = Get-FileHash "$($meta.InternalName).dll" -Algorithm SHA512
+Set-Location ..
+
+# Checkout linked repository
+$repoUrl = $meta.RepoUrl
+git clone $repoUrl
+
+# Build inside repository and get latest DLL hash
+Set-Location $repoUrl.Substring($repoUrl.LastIndexOf("/") + 1)
+dotnet restore
+dotnet build -c Release
+Set-Location "$($meta.InternalName)/bin/Release/net472"
+$hash2 = Get-FileHash "$($meta.InternalName).dll" -Algorithm SHA512
+
+# Compare DLL hashes and provide result
+if ($hash2 -eq "") {
+    Write-Error "The linked repository is set up differently than expected. This is not an issue in and of itself, but the PR process may take longer than usual."
+    return
+}
+
+if ($hash1 -ne $hash2) {
+    Write-Error "DLL checksum does not match build from linked repository. This is not an issue in and of itself, but the PR process may take longer than usual."
+    return
+}
+
+Write-Output "DLL checksum matches build from linked repository. The linked repository code can be read instead of using ILSpy."
